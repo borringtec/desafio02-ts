473a28a4d95c08963b5f2f50b72eb86e3c3a4762
Rafael Borring <borringtec@gmail.com>
2025-09-24 08:59:53 -0300

    Create header component.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,20 @@
 import './Header.css'
+import {
+    ChakraProvider,
+    Box,
+    Center
+} from '@chakra-ui/react'
 
-export const Header  = () => {
-  return(
-    <div className='header'>
-      Dio Bank
-    </div>
-  )
+export const Header = () => {
+    return (
+        <ChakraProvider>
+            <Box minHeight='10vh' backgroundColor='#9413dc' padding='25px'>
+                <Box backgroundColor='#FFFFFF' borderRadius='25px' padding='15px'>
+                    <Center>
+                        <h1>Dio Bank</h1>
+                    </Center>
+                </Box>
+            </Box>
+        </ChakraProvider>
+    );
 }
